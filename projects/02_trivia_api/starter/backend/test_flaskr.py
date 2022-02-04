2873043b18deea65b4e73be839e0a432a7b475fe
Turkibash <dev@turki.tech>
2022-02-04 04:35:23 +0300

    FIXED indentation

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -33,7 +33,7 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
-def test_get_categories(self):
+    def test_get_categories(self):
         response = self.client().get('/categories')
         data = json.loads(response.data)
 
@@ -42,7 +42,7 @@ def test_get_categories(self):
         self.assertTrue(len(data['categories']))
 
 
-def test_get_questions(self):
+    def test_get_questions(self):
         response = self.client().get('/questions')
         data = json.loads(response.data)
 
@@ -53,7 +53,7 @@ def test_get_questions(self):
         self.assertTrue(len(data['categories']))
         self.assertEqual(data['current_category'],None)
 
-def test_422_sent_requesting_beyond_valid_pages(self):
+    def test_422_sent_requesting_beyond_valid_pages(self):
         response = self.client().get('/questions?page=500')
         data = json.loads(response.data)
 
@@ -61,7 +61,7 @@ def test_422_sent_requesting_beyond_valid_pages(self):
         self.assertEqual(data['success'],False)
         self.assertTrue(data['message'],"Not Found")
 
-def test_delete_question(self):
+    def test_delete_question(self):
         response = self.client().delete('/questions/1')
         data = json.loads(response.data)
 
@@ -73,7 +73,7 @@ def test_delete_question(self):
         self.assertTrue(data['total_questions'])
         self.assertEqual(question, None)
 
-def test_404_if_question_does_not_exist(self):
+    def test_404_if_question_does_not_exist(self):
         response = self.client().delete('/questions/1000')
         data = json.loads(response.data)
 
@@ -81,7 +81,7 @@ def test_404_if_question_does_not_exist(self):
         self.assertEqual(data['success'],False)
         self.assertTrue(data['message'],"Not Found")
 
-def test_post_question(self):
+    def test_post_question(self):
         response = self.client().post('/questions', json={'question': ' Which England footballer was famously never given a yellow card?', 'answer' : 'Gary Lineker', 'difficulty' : 3, 'category' : 1})
         data = json.loads(response.data)
 
@@ -90,7 +90,7 @@ def test_post_question(self):
         self.assertTrue(data['created'])
         self.assertTrue(data['total_questions'])
 
-def test_400_if_post_question_bad_request(self):
+    def test_400_if_post_question_bad_request(self):
         response = self.client().post('/questions', json={'q': ' Which England footballer was famously never given a yellow card?', 'a' : 'Gary Lineker', 'd' : 3, 'c' : 1})
         data = json.loads(response.data)
 
@@ -98,7 +98,7 @@ def test_400_if_post_question_bad_request(self):
         self.assertEqual(data['success'],False)
         self.assertTrue(data['message'],"Bad Request")
 
-def test_search_question(self):
+    def test_search_question(self):
         response = self.client().post('/questions/search', json={'searchTerm': 'which'})
         data = json.loads(response.data)
 
@@ -108,7 +108,7 @@ def test_search_question(self):
         self.assertTrue(data['total_questions'])
         self.assertEqual(data['current_category'],None)
 
-def test_400_if_search_question_bad_request(self):
+    def test_400_if_search_question_bad_request(self):
         response = self.client().post('/questions/search', json={'search': 'which'})
         data = json.loads(response.data)
 
@@ -116,7 +116,7 @@ def test_400_if_search_question_bad_request(self):
         self.assertEqual(data['success'],False)
         self.assertTrue(data['message'],"Bad Request")   
 
-def test_get_questions_by_category(self):
+    def test_get_questions_by_category(self):
         response = self.client().get('/categories/1/questions')
         data = json.loads(response.data)
         category = Category.query.get(1)
@@ -127,7 +127,7 @@ def test_get_questions_by_category(self):
         self.assertTrue(data['total_questions'])
         self.assertEqual(data['current_category'],category.type)
 
-def test_404_sent_requesting_beyond_valid_categories(self):
+    def test_404_sent_requesting_beyond_valid_categories(self):
         response = self.client().get('/categories/1000/questions')
         data = json.loads(response.data)
 
@@ -135,7 +135,7 @@ def test_404_sent_requesting_beyond_valid_categories(self):
         self.assertEqual(data['success'],False)
         self.assertTrue(data['message'],"Not Found")     
 
-def test_get_question_quizzes(self):
+    def test_get_question_quizzes(self):
         category = Category.query.get(4)
         questions = Question.query.filter_by(category=category.id).all()
 
@@ -147,7 +147,7 @@ def test_get_question_quizzes(self):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data['success'], True)
 
-def test_400_get_question_quizzes_bad_request(self):
+    def test_400_get_question_quizzes_bad_request(self):
         category = Category.query.get(4)
         questions = Question.query.filter_by(category=category.id).all()
 
